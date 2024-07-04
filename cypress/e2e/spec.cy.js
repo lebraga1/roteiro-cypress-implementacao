6013def3f9ed9801ca623a6c2536e48e62fbe118
Leônidas Braga <lebragacool@gmail.com>
2024-07-03 22:43:12 -0300

    Implemented new E2E tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,55 @@ describe('template spec', () => {
     cy.get('.todo-list li')
       .should('have.length', 2);
   });
+
+  it('Edita uma tarefa', () => {
+    cy.visit('http://127.0.0.1:7001');
+    
+    cy.get('.new-todo')
+    .type('Limpar a cozinha{enter}');
+
+    cy.get('.todo-list li')
+      .first()
+      .dblclick();
+    
+    cy.get('.edit')
+      .clear()
+      .type('Lavar os pratos{enter}');
+
+    cy.get('.todo-list li')
+      .first()
+      .should('have.text', 'Lavar os pratos'); 
+  });
+
+  it('Limpa tarefas completas', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}')
+      
+
+    cy.get('.todo-list li .toggle')
+      .click({multiple: true});
+    
+    cy.contains("Clear completed").click();
+
+    cy.get('.todo-list li')
+      .should('have.length', 0);
+  });
+
+  it('Numero tarefas restantes', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}')
+
+    cy.get('.todo-list li .toggle')
+      .first()
+      .click();
+    
+    cy.get(".todo-count strong")
+      .should("have.text", "1");
+  });
 });
